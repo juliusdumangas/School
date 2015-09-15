d6a33f5fb56422635f6fbe3001ae76c57f57c9c8
adriell <jelaiadriell16@gmail.com>
2015-09-15 17:12:16 +0800

    whirlwind.java added

---
+++ Whirlwind.java
@@ -0,0 +1,21 @@
+/*
+ * @(#)Whirlwind.java        1.0 15/09/15
+ */
+
+import java.util.Scanner;
+
+/*
+ *      This program will prompt the user to enter
+ *      how many numbers to input. Accept the
+ *      numbers and display the sum of the numbers.
+ *      
+ * @version 1.0 15 Sep 2015  
+ * @author Julius Adriell Dumangas
+ * 
+ */
+
+public class Whirlwind {
+    public static void main(String[] args){
+        
+    }
+}
