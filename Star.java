b5b2c57d62b8e6d4c15690e75548f6ebc462d9c0
Julius Dumangas <julius.gdrive@gmail.com>
2015-09-29 21:54:55 +0800

    added star.java

--- Star.java
+++ Star.java
@@ -1,15 +1,15 @@
 /*
- * @(#)Star.java        1.0 16/09/15
+ * @(#)Star.java        1.0 29/09/15
  */
 
 import java.util.Scanner;
 
 /*
- *      This program will prompt the user to enter the
- *      height of the letter x. Display the letter x
+ *      This program will prompt the user to enter
+ *      the height of the letter "x" and display it
  *      with the required height.
  *      
- * @version 1.0 16 Sep 2015  
+ * @version 1.0 29 Sep 2015  
  * @author Julius Adriell Dumangas
  * 
  */
@@ -19,19 +19,15 @@ public class Star {
         int height;
         Scanner s = new Scanner(System.in);
 
-        while (true){
-            System.out.print("\nEnter height from 3 to 15: ");
-            height = s.nextInt();
-            if (height < 3 || height > 15 || (height % 2 == 0)){
-                System.out.print("\nERROR: Must be an odd number and from 3 to 15.");
-            }else{
-                break;
-            }
-        }
+        System.out.print("\nEnter height from 3 to 15: ");
+        height = s.nextInt();
+
         for (int i = 1; i <= height; i++){
             for (int j = 1; j <= height; j++){
-                if (i % 2 == 0){
-                    
+                if (j == i || j == (height+1-i)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
                 }
             }
             System.out.println();
